e6956e15cd6c0a2e003a442880e4c6f4956f7bb0
ezen <ezen@DESKTOP-O03MT84>
2022-02-03 17:27:41 +0900

    2

--- src/com/google/lecture/ch02/Hello.java
+++ src/com/google/lecture/ch02/Hello.java
@@ -6,6 +6,19 @@ public class Hello {
 		// TODO Auto-generated method stub
 
 		System.out.println("반가워요");
+		
+		int a; // 선업 대입(초기화)
+		a=-1;
+		int b=2;
+		
+		int c=0;
+		
+		
+		double d=3.4;
+		
+		c=a+b;
+		
+		System.out.println(c);
 	}
 
 }
