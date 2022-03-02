450758575335cecb60f8d03a6ad0357d603fe2b1
sjrnfl23 <tnsgud1010@m365.dongyang.ac.kr>
2022-03-02 15:22:23 +0900

    2

---
+++ src/ch07/StudentExample.java
@@ -0,0 +1,14 @@
+package ch07;
+
+public class StudentExample {
+
+	public static void main(String[] args) {
+	
+		Student student =new Student("Tony","1234-1234",21);
+		
+		System.out.println(student.name);
+		System.out.println(student.ssn);
+		System.out.println(student.studentNo);
+	}
+
+}
